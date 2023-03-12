8491b669e921db08c4c7f8c473b09face8b79f45
Nikita <nikita9638697736@gmail.com>
2023-03-12 18:08:13 +0300

    'lesson9'

--- women/views.py
+++ women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect, render
 
 from .models import *
@@ -13,10 +13,13 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 def index(request):
     posts = WOmen.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -39,6 +42,23 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f'Отображение статьи с id = {post_id}')
+
+
+def show_category(request, cat_id):
+    posts = WOmen.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):
